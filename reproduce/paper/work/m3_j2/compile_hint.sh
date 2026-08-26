652bb5f27033bc7a89f621cf285447762f9480e5
santiagarcia <50498856+santiagarcia@users.noreply.github.com>
2026-08-26 10:16:19 -0500

    Ignore reproduction output
    A `git add -A` swept a profile run into the tree, and with it the absolute
    paths of the machine that produced it.

--- reproduce/paper/work/m3_j2/compile_hint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-OBJDIR=${OBJDIR:-.}
-gfortran -c -ffree-form -ffree-line-length-none master_parameters.f90 -J"$OBJDIR" -o "$OBJDIR/master_parameters.o"
-gfortran -c -ffree-form -ffree-line-length-none -I"$OBJDIR" real_utils.f90 -J"$OBJDIR" -o "$OBJDIR/real_utils.o"
-gfortran -c -ffree-form -ffree-line-length-none -I"$OBJDIR" otim6n1.f90 -J"$OBJDIR" -o "$OBJDIR/otim6n1.o"
-gfortran -c -ffixed-form -ffixed-line-length-none -I"$OBJDIR" umat_oti.for -J"$OBJDIR" -o "$OBJDIR/transformed_umat.o"
